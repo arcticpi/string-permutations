d8198a118fb9b0110db12d427cac02c646abe537
Giacomo Garbin <giacomogarbinmailbox@gmail.com>
2019-04-09 12:04:53 +0200

    created function permutations

--- string-permutations.cpp
+++ string-permutations.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 using namespace std;
 
+void permutations(std::string string)
+{
+    std::cout << string << std::endl;
+}
+
 int main()
 {
     std::cout << "string permutations!" << std::endl;
+    permutations("kulaworld");
     return 0;
 }
