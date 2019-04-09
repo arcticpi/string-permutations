8c6cc87609c202f6136d05245c83f51a7b841f21
Giacomo Garbin <giacomogarbinmailbox@gmail.com>
2019-04-09 12:21:48 +0200

    updated string-permutations.cpp

--- string-permutations.cpp
+++ string-permutations.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
 using namespace std;
 
-void permutations(std::string string)
+void permutations(std::string string, std::string prefix)
 {
-    std::cout << string << std::endl;
+    if (string.length() == 0)
+    {
+        std::cout << prefix << std::endl;
+    }
+    else
+    {
+        for (int i = 0; i < string.length(); i++)
+        {
+            std::string postfix = string.substr(0, i) + string.substr(i + 1);
+            permutations(postfix, prefix + string.at(i));
+        }
+    }
 }
 
 int main()
 {
-    std::cout << "string permutations!" << std::endl;
-    permutations("kulaworld");
+    permutations("abcd", "");
     return 0;
 }
